dcf976230f4c5dfcb7d798f5045bd1767fa9721c
Aryan Shah <aryanwork10@gmail.com>
2023-07-29 00:43:05 +0530

    ninth commit

--- src/pages/PlacesForm.js
+++ src/pages/PlacesForm.js
@@ -172,7 +172,7 @@ function PlacesForm(){
         // });
         const publicId = form.photos[index].split('/').slice(-2).join('/').replace(/\.[^/.]+$/, '');
 
-        const response=await fetch(`https://airbnb-clone-backend-one.vercel.app/deletePhoto/${publicId}`, {
+        const response=await fetch(`https://airbnb-clone-backend-one.vercel.app/deletePhoto`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
